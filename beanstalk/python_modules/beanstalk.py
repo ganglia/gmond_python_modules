1cc58cbe7aff89f5784d3690ab1e6b6d611f0d34
Stephen Holiday <stephen.holiday@gmail.com>
2010-12-11 11:37:03 -0800

    Add metrics for individual tubes

--- beanstalk/python_modules/beanstalk.py
+++ beanstalk/python_modules/beanstalk.py
@@ -2,9 +2,15 @@
 # -*- coding: utf-8 -*-
 import beanstalkc
 
+HOST='localhost'
+PORT=14711
 def stat_handler(name):
-    bean=beanstalkc.Connection(host='localhost',port=14711)
+    bean=beanstalkc.Connection(host=HOST,port=PORT)
     return bean.stats()[name]
+    
+def tube_stat_handler(name):
+    bean=beanstalkc.Connection(host=HOST,port=PORT)
+    return bean.stats_tube(name.split('_')[0])[name.split('_')[1]]
     
 def metric_init(params):
     global descriptors
@@ -82,7 +88,62 @@ def metric_init(params):
             'description': 'Number of Beanstalkd Burries',
             'groups': 'beanstalkd'}
         ]
-
+        
+    #now get all the tubes
+    bean=beanstalkc.Connection(host=HOST,port=PORT)
+    tubes=bean.tubes()
+    for tube in tubes:
+        descriptors.append(
+        {'name': tube+'_total-jobs',
+            'call_back': tube_stat_handler,
+            'time_max': 90,
+            'value_type': 'uint',
+            'units': 'total jobs',
+            'slope': 'both',
+            'format': '%u',
+            'description': 'Number of Beanstalkd Jobs ('+tube+')',
+            'groups': 'beanstalkd'})
+        descriptors.append(
+        {'name': tube+'_current-watching',
+            'call_back': tube_stat_handler,
+            'time_max': 90,
+            'value_type': 'uint',
+            'units': 'clients',
+            'slope': 'both',
+            'format': '%u',
+            'description': 'Number Watchers ('+tube+')',
+            'groups': 'beanstalkd'})
+        descriptors.append(
+        {'name': tube+'_current-jobs-buried',
+            'call_back': tube_stat_handler,
+            'time_max': 90,
+            'value_type': 'uint',
+            'units': 'jobs',
+            'slope': 'both',
+            'format': '%u',
+            'description': 'Current Number of Jobs Burried ('+tube+')',
+            'groups': 'beanstalkd'})
+        descriptors.append(
+        {'name': tube+'_current-jobs-ready',
+            'call_back': tube_stat_handler,
+            'time_max': 90,
+            'value_type': 'uint',
+            'units': 'clients',
+            'slope': 'both',
+            'format': '%u',
+            'description': 'Current Jobs Ready ('+tube+')',
+            'groups': 'beanstalkd'})
+        descriptors.append(
+        {'name': tube+'_current-waiting',
+            'call_back': tube_stat_handler,
+            'time_max': 90,
+            'value_type': 'uint',
+            'units': 'jobs',
+            'slope': 'both',
+            'format': '%u',
+            'description': 'Current Number of Jobs Waiting ('+tube+')',
+            'groups': 'beanstalkd'})
+    
     return descriptors
 
 def metric_cleanup():
