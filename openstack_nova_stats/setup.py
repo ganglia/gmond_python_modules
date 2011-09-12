08afd6c4a572d796f428c6b708838fdd6d5d51d4
Nikolay Sokolov <nsokolov@griddynamics.com>
2011-09-12 17:27:39 +0400

    Fixed config file collision

--- openstack_nova_stats/setup.py
+++ openstack_nova_stats/setup.py
@@ -28,7 +28,7 @@ setup(
     author_email = 'nsokolov@griddynamics.com',
     data_files = [('/usr/local/share/openstack_nova_stats/graph.d', allfiles(os.path.join(os.path.dirname(__file__), 'graph.d'))),
                   ('/usr/local/share/doc/openstack_nova_stats', ['README']),
-                  ('/etc/ganglia/conf.d', allfiles(os.path.join(os.path.dirname(__file__), 'conf.d'))),
+                  ('/etc/ganglia/conf.d', ['conf.d/compute-metrics.pyconf']),
                   ('/etc/ganglia/python_modules', allfiles(os.path.join(os.path.dirname(__file__), 'python_modules')))],
     classifiers = [
         'Development Status :: 3 - Alpha',
