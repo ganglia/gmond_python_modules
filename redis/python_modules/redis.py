dd780a70f426d5011f87d229d5e72e7b027cf77b
Ben Hartshorne <ben@parse.com>
2012-11-06 14:37:58 -0800

    upgrading redis module to work with redis 2.6.  Not backwards compatible.

--- redis/python_modules/redis.py
+++ redis/python_modules/redis.py
@@ -1,10 +1,9 @@
-"""
-Redis in Ganglia
-Richard Crowley <richard@devstructure.com>
-"""
-
 import socket
 import time
+#import logging
+
+#logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s\t Thread-%(thread)d - %(message)s", filename='/tmp/gmond.log', filemode='w')
+#logging.debug('starting up')
 
 def metric_handler(name):
 
@@ -12,53 +11,61 @@ def metric_handler(name):
     if 15 < time.time() - metric_handler.timestamp:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((metric_handler.host, metric_handler.port))
-        s.settimeout(5) # set socket timeout as to not block gmond
-        # if the password is set from parameters
-        if metric_handler.password != None:
-        	s.send("AUTH {0}\r\n".format(metric_handler.password))
-        	s.recv(4096) # TODO check if auth is valid
         s.send("INFO\r\n")
+        #logging.debug("sent INFO")
         info = s.recv(4096)
+        #logging.debug("rcvd INFO")
         if "$" != info[0]:
             return 0
         len = int(info[1:info.find("\n")])
         if 4096 < len:
             info += s.recv(len - 4096)
         metric_handler.info = {}
-        for line in info.splitlines()[1:]:
-            if "" == line:
-                continue
-            if "#" == line[0]:
-                continue
-            n, v = line.split(":")
-            if n in metric_handler.descriptors:
-                metric_handler.info[n] = int(v) # TODO Use value_type.
+        try:
+          for line in info.splitlines()[1:]:
+              #logging.debug("line is %s done" % line)
+              if "" == line:
+                  continue
+              if "#" == line[0]:
+                  continue
+              n, v = line.split(":")
+              if n in metric_handler.descriptors:
+                  if n == "master_sync_status":
+                      v = 1 if v == 'up' else 0
+                  if n == "db0":
+                      v = v.split('=')[1].split(',')[0]
+                  if n == "used_memory":
+                      v = int(int(v) / 1000)
+                  #logging.debug("submittincg metric %s is %s" % (n, int(v)))
+                  metric_handler.info[n] = int(v) # TODO Use value_type.
+        except Exception, e:
+            #logging.debug("caught exception %s" % e)
+            pass
         s.close()
         metric_handler.timestamp = time.time()
 
+    #logging.debug("returning metric_handl: %s %s %s" % (metric_handler.info.get(name, 0), metric_handler.info, metric_handler))
     return metric_handler.info.get(name, 0)
 
 def metric_init(params={}):
     metric_handler.host = params.get("host", "127.0.0.1")
     metric_handler.port = int(params.get("port", 6379))
-    metric_handler.password = params.get("password", None)
     metric_handler.timestamp = 0
     metrics = {
         "connected_clients": {"units": "clients"},
         "connected_slaves": {"units": "slaves"},
         "blocked_clients": {"units": "clients"},
-        "used_memory": {
-            "units": "bytes",
-            "value_type": "double",
-            "format": "%f",
-        },
-        "changes_since_last_save": {"units": "changes"},
-        "bgsave_in_progress": {"units": "yes/no"},
-        "bgrewriteaof_in_progress": {"units": "yes/no"},
+        "used_memory": {"units": "KB"},
+        "rdb_changes_since_last_save": {"units": "changes"},
+        "rdb_bgsave_in_progress": {"units": "yes/no"},
+        "master_sync_in_progress": {"units": "yes/no"},
+        "master_link_status": {"units": "yes/no"},
+        #"aof_bgrewriteaof_in_progress": {"units": "yes/no"},
         "total_connections_received": {
             "units": "connections",
             "slope": "positive",
         },
+        "instantaneous_ops_per_sec": {"units": "ops"},
         "total_commands_processed": {
             "units": "commands",
             "slope": "positive",
@@ -66,7 +73,9 @@ def metric_init(params={}):
         "expired_keys": {"units": "keys"},
         "pubsub_channels": {"units": "channels"},
         "pubsub_patterns": {"units": "patterns"},
+        #"vm_enabled": {"units": "yes/no"},
         "master_last_io_seconds_ago": {"units": "seconds ago"},
+        "db0": {"units": "keys"},
     }
     metric_handler.descriptors = {}
     for name, updates in metrics.iteritems():
@@ -74,7 +83,7 @@ def metric_init(params={}):
             "name": name,
             "call_back": metric_handler,
             "time_max": 90,
-            "value_type": "uint",
+            "value_type": "int",
             "units": "",
             "slope": "both",
             "format": "%d",
@@ -87,10 +96,3 @@ def metric_init(params={}):
 
 def metric_cleanup():
     pass
-
-# For testing
-if __name__ == "__main__":
-    desc = metric_init({"host": "127.0.0.1"})
-    for d in desc:
-        v = d['call_back'](d['name'])
-        print 'value for %s is %f' % (d['name'], v)
