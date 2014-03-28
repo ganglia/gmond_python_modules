c05c7eedac2bc619ed760c469c803aafb25f3af5
Evan Fraser <evanjfraser@gmail.com>
2014-03-28 17:08:52 +1300

    Fixed volume latency units, re-arranged module

--- netapp_api/python_modules/netapp_cmode_api.py
+++ netapp_api/python_modules/netapp_cmode_api.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-#Name: netapp_api_cmode.py
+#Name: netapp_api.py
 #Desc: Uses Netapp Data Ontap API to get per volume latency & iops metrics.  Download the managemability SDK from now.netapp.com
 #Author: Evan Fraser <evan.fraser@trademe.co.nz>
 #Date: 13/08/2012
@@ -33,6 +33,8 @@ class GetMetricsThread(threading.Thread):
         self.filer_metrics = None
         self.MetricName = MetricName
         self.FilerName = FilerName
+        self.instances = None
+        self.ClusterName = filerdict[self.FilerName]['name']
         super(GetMetricsThread, self).__init__()
     
     def run(self):
@@ -92,12 +94,18 @@ class GetMetricsThread(threading.Thread):
         if(out.results_status() == "failed"):
             print(out.results_reason() + "\n")
             sys.exit(2)
+
+        #self.clusterName = filerdict[filer]['name']
     
-        filername = filerdict[filer]['name']
         instances_list = out.child_get("instances")            
         instances = instances_list.children_get()
+        self.instances = instances
 
-        for inst in instances:
+    #Function within the class for updating the metrics
+    def update_metrics(self):
+        clustername = self.ClusterName
+        print clustername
+        for inst in self.instances:
             inst_name = unicodedata.normalize('NFKD',inst.child_get_string("name")).encode('ascii','ignore')
             counters_list = inst.child_get("counters")
             counters = counters_list.children_get()
@@ -106,7 +114,71 @@ class GetMetricsThread(threading.Thread):
                 counter_name = unicodedata.normalize('NFKD',counter.child_get_string("name")).encode('ascii','ignore')         
                 counter_value = counter.child_get_string("value")
                 counter_unit = counter.child_get_string("unit")           
-                self.filer_metrics[filername + '_vol_' + inst_name + '_' + counter_name] = float(counter_value)
+                self.filer_metrics[clustername + '_vol_' + inst_name + '_' + counter_name] = float(counter_value)
+
+    #Function within the class for defining the metrics for ganglia
+    def define_metrics(self,Desc_Skel,params):
+        
+        clustername = self.ClusterName
+        filer = self.FilerName
+        for inst in self.instances:
+            inst_name = unicodedata.normalize('NFKD',inst.child_get_string("name")).encode('ascii','ignore')
+            counters_list = inst.child_get("counters")
+            counters = counters_list.children_get()
+
+            for counter in counters:
+                counter_name = unicodedata.normalize('NFKD',counter.child_get_string("name")).encode('ascii','ignore')
+                counter_value = counter.child_get_string("value")
+                counter_unit = counter.child_get_string("unit")
+                print counter_name
+                if 'total_ops' in counter_name:
+                    descriptors.append(create_desc(Desc_Skel, {
+                                "name"        : clustername + '_vol_' + inst_name + '_' + counter_name,
+                                "units"       : 'iops',
+                                "description" : "volume iops",
+                                "spoof_host"  : params[filer]['ipaddr'] + ':' + params[filer]['name'],
+                                "groups"      : "iops"
+                                }))
+                elif 'avg_latency' in counter_name:
+                    descriptors.append(create_desc(Desc_Skel, {
+                                "name"        : clustername + '_vol_' + inst_name + '_' + counter_name,
+                                "units"       : 'ms',
+                                "description" : "volume avg latency",
+                                "spoof_host"  : params[filer]['ipaddr'] + ':' + params[filer]['name'],
+                                "groups"      : "latency"
+                                }))
+                elif 'read_ops' in counter_name:
+                    descriptors.append(create_desc(Desc_Skel, {
+                                "name"        : clustername + '_vol_' + inst_name + '_' + counter_name,
+                                "units"       : 'iops',
+                                "description" : "volume read iops",
+                                "spoof_host"  : params[filer]['ipaddr'] + ':' + params[filer]['name'],
+                                "groups"      : "iops"
+                                }))
+                elif 'read_latency' in counter_name:
+                    descriptors.append(create_desc(Desc_Skel, {
+                                "name"        : clustername + '_vol_' + inst_name + '_' + counter_name,
+                                "units"       : 'ms',
+                                "description" : "volume read latency",
+                                "spoof_host"  : params[filer]['ipaddr'] + ':' + params[filer]['name'],
+                                "groups"      : "latency"
+                                }))
+                elif 'write_ops' in counter_name:
+                    descriptors.append(create_desc(Desc_Skel, {
+                                "name"        : clustername + '_vol_' + inst_name + '_' + counter_name,
+                                "units"       : 'iops',
+                                "description" : "volume write iops",
+                                "spoof_host"  : params[filer]['ipaddr'] + ':' + params[filer]['name'],
+                                "groups"      : "iops"
+                                }))
+                elif 'write_latency' in counter_name:
+                    descriptors.append(create_desc(Desc_Skel, {
+                                "name"        : clustername + '_vol_' + inst_name + '_' + counter_name,
+                                "units"       : 'ms',
+                                "description" : "volume write latency",
+                                "spoof_host"  : params[filer]['ipaddr'] + ':' + params[filer]['name'],
+                                "groups"      : "latency"
+                                }))
 
         
 def get_metrics(name):
@@ -125,6 +197,7 @@ def get_metrics(name):
         #Wait for the threads to return here
         for t in threads:
             t.join()
+            t.update_metrics()
             metrics.update(t.filer_metrics)
         #end = time.time()
         #print "elapsed time was: ",(end - start)
@@ -157,7 +230,7 @@ def get_metrics(name):
             #Find the metric name of the base counter
             total_ops_name = name.replace('avg_latency', 'total_ops')
             #Calculate latency in time (div 100 to change to ms)
-            return float((FASMETRICS['data'][name] - LAST_FASMETRICS['data'][name]) / (FASMETRICS['data'][total_ops_name] -LAST_FASMETRICS['data'][total_ops_name])) / 100
+            return float((FASMETRICS['data'][name] - LAST_FASMETRICS['data'][name]) / (FASMETRICS['data'][total_ops_name] -LAST_FASMETRICS['data'][total_ops_name])) / 1000
         except StandardError:
             return 0
     elif 'read_ops' in name:
@@ -174,7 +247,7 @@ def get_metrics(name):
     elif 'read_latency' in name:
         try: 
             read_ops_name = name.replace('read_latency', 'read_ops')
-            return float((FASMETRICS['data'][name] - LAST_FASMETRICS['data'][name]) / (FASMETRICS['data'][read_ops_name] -LAST_FASMETRICS['data'][read_ops_name])) / 100
+            return float((FASMETRICS['data'][name] - LAST_FASMETRICS['data'][name]) / (FASMETRICS['data'][read_ops_name] -LAST_FASMETRICS['data'][read_ops_name])) / 1000
         except StandardError:
             return 0
     elif 'write_ops' in name:
@@ -190,7 +263,7 @@ def get_metrics(name):
     elif 'write_latency' in name:
         try: 
             write_ops_name = name.replace('write_latency', 'write_ops')
-            return float((FASMETRICS['data'][name] - LAST_FASMETRICS['data'][name]) / (FASMETRICS['data'][write_ops_name] -LAST_FASMETRICS['data'][write_ops_name])) / 100
+            return float((FASMETRICS['data'][name] - LAST_FASMETRICS['data'][name]) / (FASMETRICS['data'][write_ops_name] -LAST_FASMETRICS['data'][write_ops_name])) / 1000
         except StandardError:
             return 0
             
@@ -206,147 +279,24 @@ def create_desc(skel, prop):
     return d
     
 
-def define_metrics_thread(Desc_Skel,filer,params):
-    global descriptors
-    s = NaServer(params[filer]['ipaddr'], 1, 3)
-    out = s.set_transport_type('HTTPS')
-    if (out and out.results_errno() != 0) :
-        r = out.results_reason()
-        print ("Connection to filer failed: " + r + "\n")
-        sys.exit(2)
-            
-    out = s.set_style('LOGIN')
-    if (out and out.results_errno() != 0) :
-        r = out.results_reason()
-        print ("Connection to filer failed: " + r + "\n")
-        sys.exit(2)
-    out = s.set_admin_user(params[filer]['user'], params[filer]['password'])
-
-    #In C-mode, perf-object-get-instances-iter-start doesn't exist
-    # Also need to get list of instance names to provide to the perf-object-get-instances now.
-    #Get list of volume instances
-    obj_name = "volume"
-    instance_in = NaElement("perf-object-instance-list-info-iter")
-    instance_in.child_add_string("objectname", obj_name)
-
-    #Invoke API
-    out = s.invoke_elem(instance_in)
-    if(out.results_status() == "failed"):
-        print("Invoke failed: " + out.results_reason() + "\n")
-        sys.exit(2)
-        
-    #create an object for all the instances which we will pass to the perf-object-get-instances below
-    instance_obj = NaElement("instances")
-    instance_list = out.child_get("attributes-list")
-    instances = instance_list.children_get()
-    instance_names = []
-    for i in instances:
-        instance_obj.child_add_string("instance", i.child_get_string("name"))
-
-            
-    #Get perf objects for each instance
-    perf_in = NaElement("perf-object-get-instances")
-    perf_in.child_add_string("objectname", obj_name)
-    perf_in.child_add(instance_obj)
-    #perf_in.
-
-    #Hard coded volume, only volume stats gathered at present
-    #Create object of type counters
-    counters = NaElement("counters")
-    #Add counter names to the object
-    counter_name_list = ["total_ops","avg_latency","read_ops","read_latency","write_ops","write_latency"]
-    for c in counter_name_list:
-        counters.child_add_string("counter", c)
-        perf_in.child_add(counters)
-        
-
-    #Invoke API
-    out = s.invoke_elem(perf_in)
-    if(out.results_status() == "failed"):
-        print("Invoke failed: " + out.results_reason() + "\n")
-        sys.exit(2)
-
-
-    #iter_tag = out.child_get_string("tag")
-    #num_records = 1
-    filername = params[filer]['name']
-
-    instances_list = out.child_get("instances")            
-    instances = instances_list.children_get()
-
-    for inst in instances:
-        inst_name = unicodedata.normalize('NFKD',inst.child_get_string("name")).encode('ascii','ignore')
-        counters_list = inst.child_get("counters")
-        counters = counters_list.children_get()
-
-        for counter in counters:
-            counter_name = unicodedata.normalize('NFKD',counter.child_get_string("name")).encode('ascii','ignore')
-            counter_value = counter.child_get_string("value")
-            counter_unit = counter.child_get_string("unit")
-            if 'total_ops' in counter_name:
-                descriptors.append(create_desc(Desc_Skel, {
-                            "name"        : filername + '_vol_' + inst_name + '_' + counter_name,
-                            "units"       : 'iops',
-                            "description" : "volume iops",
-                            "spoof_host"  : params[filer]['ipaddr'] + ':' + params[filer]['name'],
-                            "groups"      : "iops"
-                            }))
-            elif 'avg_latency' in counter_name:
-                descriptors.append(create_desc(Desc_Skel, {
-                            "name"        : filername + '_vol_' + inst_name + '_' + counter_name,
-                            "units"       : 'ms',
-                            "description" : "volume avg latency",
-                            "spoof_host"  : params[filer]['ipaddr'] + ':' + params[filer]['name'],
-                            "groups"      : "latency"
-                            }))
-            elif 'read_ops' in counter_name:
-                descriptors.append(create_desc(Desc_Skel, {
-                            "name"        : filername + '_vol_' + inst_name + '_' + counter_name,
-                            "units"       : 'iops',
-                            "description" : "volume read iops",
-                            "spoof_host"  : params[filer]['ipaddr'] + ':' + params[filer]['name'],
-                            "groups"      : "iops"
-                            }))
-            elif 'read_latency' in counter_name:
-                descriptors.append(create_desc(Desc_Skel, {
-                            "name"        : filername + '_vol_' + inst_name + '_' + counter_name,
-                            "units"       : 'ms',
-                            "description" : "volume read latency",
-                            "spoof_host"  : params[filer]['ipaddr'] + ':' + params[filer]['name'],
-                            "groups"      : "latency"
-                            }))
-            elif 'write_ops' in counter_name:
-                descriptors.append(create_desc(Desc_Skel, {
-                            "name"        : filername + '_vol_' + inst_name + '_' + counter_name,
-                            "units"       : 'iops',
-                            "description" : "volume write iops",
-                            "spoof_host"  : params[filer]['ipaddr'] + ':' + params[filer]['name'],
-                            "groups"      : "iops"
-                            }))
-            elif 'write_latency' in counter_name:
-                descriptors.append(create_desc(Desc_Skel, {
-                            "name"        : filername + '_vol_' + inst_name + '_' + counter_name,
-                            "units"       : 'ms',
-                            "description" : "volume write latency",
-                            "spoof_host"  : params[filer]['ipaddr'] + ':' + params[filer]['name'],
-                            "groups"      : "latency"
-                            }))
-    #return descriptors
     
 
 def define_metrics(Desc_Skel,params):
     global descriptors
+    ObjectTypeList = ["lif:vserver"]
     max_records = 10
     threads = []
     for filer in params.keys():
         #call define_metrics_thread as separate threads for each filer
-        thread = threading.Thread(target=define_metrics_thread, args=(Desc_Skel,filer,params))
+        blankname = ""
+        thread = GetMetricsThread(blankname,filer)
         thread.start()
         threads.append(thread)
 
     for t in threads:
         t.join()
-    
+        t.define_metrics(Desc_Skel,params)
+            
     return descriptors
 
 def metric_init(params):
@@ -354,15 +304,15 @@ def metric_init(params):
     print 'netapp_stats] Received the following parameters'
     params = {
         'filer1' : {
-            'name' : 'cluster1mgmt.local',
+            'name' : 'cluster1.localdomain',
             'ipaddr' : '192.168.1.100',
-            'user' : 'monitoruser',
+            'user' : 'username',
             'password' : 'password',
               },
         'filer2' : {
-            'name' : 'cluster2mgmt.local',
-            'ipaddr' : '192.168.1.200',
-            'user' : 'monitoruser',
+            'name' : 'cluster2.localdomain',
+            'ipaddr' : '192.168.1.100',
+            'user' : 'username',
             'password' : 'password',
               },
         }
