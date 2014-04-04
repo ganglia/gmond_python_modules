9953654f32e36c4ad985f36a08c5681d04ca7a30
cburroughs <chris.burroughs@gmail.com>
2014-04-04 15:07:28 -0400

    mostly conforment to pep8

--- rabbit/python_modules/rabbitmq.py
+++ rabbit/python_modules/rabbitmq.py
@@ -27,7 +27,7 @@ stats = {}
 keyToPath = {}
 last_update = None
 #last_update = {}
-compiled_results = {"nodes" : None, "queues" : None, "connections" : None, "exchanges": None}
+compiled_results = {"nodes": None, "queues": None, "connections": None, "exchanges": None}
 #Make initial stat test time dict
 #for stat_type in ('queues', 'connections','exchanges', 'nodes'):
 #    last_update[stat_type] = None
@@ -57,15 +57,15 @@ RATE_METRICS = [
 ]
 
 QUEUE_METRICS = ['rmq_messages_ready',
-                'rmq_messages_unacknowledged',
-                'rmq_backing_queue_ack_egress_rate',
-                'rmq_backing_queue_ack_ingress_rate',
-                'rmq_backing_queue_egress_rate',
-                'rmq_backing_queue_ingress_rate',
-                'rmq_backing_queue_mirror_senders',
-                'rmq_memory',
-                'rmq_consumers',
-                'rmq_messages']
+                 'rmq_messages_unacknowledged',
+                 'rmq_backing_queue_ack_egress_rate',
+                 'rmq_backing_queue_ack_ingress_rate',
+                 'rmq_backing_queue_egress_rate',
+                 'rmq_backing_queue_ingress_rate',
+                 'rmq_backing_queue_mirror_senders',
+                 'rmq_memory',
+                 'rmq_consumers',
+                 'rmq_messages']
 
 # NODE METRICS #
 keyToPath['rmq_disk_free'] = "%s{0}disk_free".format(JSON_PATH_SEPARATOR)
@@ -75,11 +75,11 @@ keyToPath['rmq_fd_used'] = "%s{0}fd_used".format(JSON_PATH_SEPARATOR)
 keyToPath['rmq_mem_used'] = "%s{0}mem_used".format(JSON_PATH_SEPARATOR)
 keyToPath['rmq_proc_used'] = "%s{0}proc_used".format(JSON_PATH_SEPARATOR)
 keyToPath['rmq_sockets_used'] = "%s{0}sockets_used".format(JSON_PATH_SEPARATOR)
-keyToPath['rmq_mem_alarm'] = "%s{0}mem_alarm".format(JSON_PATH_SEPARATOR) #Boolean
+keyToPath['rmq_mem_alarm'] = "%s{0}mem_alarm".format(JSON_PATH_SEPARATOR)  # Boolean
 keyToPath['rmq_mem_binary'] = "%s{0}mem_binary".format(JSON_PATH_SEPARATOR)
 keyToPath['rmq_mem_code'] = "%s{0}mem_code".format(JSON_PATH_SEPARATOR)
 keyToPath['rmq_mem_proc_used'] = "%s{0}mem_proc_used".format(JSON_PATH_SEPARATOR)
-keyToPath['rmq_running'] = "%s{0}running".format(JSON_PATH_SEPARATOR) #Boolean
+keyToPath['rmq_running'] = "%s{0}running".format(JSON_PATH_SEPARATOR)  # Boolean
 
 NODE_METRICS = ['rmq_disk_free', 'rmq_mem_used', 'rmq_disk_free_alarm', 'rmq_running', 'rmq_proc_used', 'rmq_mem_proc_used', 'rmq_fd_used', 'rmq_mem_alarm', 'rmq_mem_code', 'rmq_mem_binary', 'rmq_sockets_used']
 
@@ -94,18 +94,19 @@ EXCHANGE_METRICS = ['rmq_exchange_publish_in_rate', 'rmq_exchange_publish_out_ra
 def metric_cleanup():
     pass
 
-def dig_it_up(obj,path):
+
+def dig_it_up(obj, path):
     try:
         path = path.split(JSON_PATH_SEPARATOR)
-        return reduce(lambda x,y:x[y],path,obj)
+        return reduce(lambda x, y: x[y], path, obj)
     except Exception, e:
         # not WARN because the False return is used for control flow
         # (zero assumed)
         log.debug('dig_it_up Exception %r  path: %s' % (e, path))
         return False
 
-def refreshStats(stats = ('nodes', 'queues'), vhosts = ['/']):
 
+def refreshStats(stats=('nodes', 'queues'), vhosts=['/']):
     global url_template
     global last_update, url, compiled_results
 
@@ -124,7 +125,7 @@ def refreshStats(stats = ('nodes', 'queues'), vhosts = ['/']):
                 if stat in ('nodes'):
                     vhost = '/'
                 result_dict = {}
-                urlstring = url_template.safe_substitute(stats = stat, vhost = vhost)
+                urlstring = url_template.safe_substitute(stats=stat, vhost=vhost)
                 log.debug('urlspring: %s' % urlstring)
                 result = json.load(urllib2.urlopen(urlstring))
                 # Rearrange results so entry is held in a dict keyed by name - queue name, host name, etc.
@@ -143,15 +144,18 @@ def validatedResult(value):
     else:
         return None
 
+
 def list_queues(vhost):
     global compiled_results
     queues = compiled_results[('queues', vhost)].keys()
     return queues
 
+
 def list_nodes():
     global compiled_results
     nodes = compiled_results[('nodes', '/')].keys()
     return nodes
+
 
 def list_exchanges(vhost):
     global compiled_results
@@ -160,24 +164,24 @@ def list_exchanges(vhost):
 
 
 def getQueueStat(name):
-    refreshStats(stats = STATS, vhosts = vhosts)
-    #Split a name like "rmq_backing_queue_ack_egress_rate.access"
+    refreshStats(stats=STATS, vhosts=vhosts)
+    # Split a name like "rmq_backing_queue_ack_egress_rate.access"
 
-    #handle queue names with . in them
+    # handle queue names with . in them
 
     log.debug(name)
     stat_name, queue_name, vhost = name.split(METRIC_TOKEN_SEPARATOR)
 
-    vhost = vhost.replace('-', '/') #decoding vhost from metric name
+    vhost = vhost.replace('-', '/')  # decoding vhost from metric name
     # Run refreshStats to get the result object
     result = compiled_results[('queues', vhost)]
 
     value = dig_it_up(result, keyToPath[stat_name] % queue_name)
 
-    if zero_rates_when_idle and stat_name in RATE_METRICS  and 'idle_since' in result[queue_name].keys():
+    if zero_rates_when_idle and stat_name in RATE_METRICS and 'idle_since' in result[queue_name].keys():
         value = 0
 
-    #Convert Booleans
+    # Convert Booleans
     if value is True:
         value = 1
     elif value is False:
@@ -185,17 +189,18 @@ def getQueueStat(name):
 
     return float(value)
 
+
 def getNodeStat(name):
-    refreshStats(stats = STATS, vhosts = vhosts)
-    #Split a name like "rmq_backing_queue_ack_egress_rate.access"
+    refreshStats(stats=STATS, vhosts=vhosts)
+    # Split a name like "rmq_backing_queue_ack_egress_rate.access"
     stat_name, node_name, vhost = name.split(METRIC_TOKEN_SEPARATOR)
-    vhost = vhost.replace('-', '/') #decoding vhost from metric name
+    vhost = vhost.replace('-', '/')  # decoding vhost from metric name
 
     result = compiled_results[('nodes', '/')]
     value = dig_it_up(result, keyToPath[stat_name] % node_name)
 
     log.debug('name: %r value: %r' % (name, value))
-    #Convert Booleans
+    # Convert Booleans
     if value is True:
         value = 1
     elif value is False:
@@ -203,25 +208,26 @@ def getNodeStat(name):
 
     return float(value)
 
-def getExchangeStat(name):
-    refreshStats(stats = STATS, vhosts = vhosts)
-    #Split a name like "rmq_backing_queue_ack_egress_rate.access"
 
-    #handle queue names with . in them
+def getExchangeStat(name):
+    refreshStats(stats=STATS, vhosts=vhosts)
+    # Split a name like "rmq_backing_queue_ack_egress_rate.access"
+
+    # handle queue names with . in them
 
     log.debug(name)
     stat_name, exchange_name, vhost = name.split(METRIC_TOKEN_SEPARATOR)
 
-    vhost = vhost.replace('-', '/') #decoding vhost from metric name
+    vhost = vhost.replace('-', '/')  # decoding vhost from metric name
     # Run refreshStats to get the result object
     result = compiled_results[('exchanges', vhost)]
 
     value = dig_it_up(result, keyToPath[stat_name] % exchange_name)
 
-    if zero_rates_when_idle and stat_name in RATE_METRICS  and 'idle_since' in result[exchange_name].keys():
+    if zero_rates_when_idle and stat_name in RATE_METRICS and 'idle_since' in result[exchange_name].keys():
         value = 0
 
-    #Convert Booleans
+    # Convert Booleans
     if value is True:
         value = 1
     elif value is False:
@@ -236,9 +242,10 @@ def product(*args, **kwds):
     pools = map(tuple, args) * kwds.get('repeat', 1)
     result = [[]]
     for pool in pools:
-        result = [x+[y] for x in result for y in pool]
+        result = [x + [y] for x in result for y in pool]
     for prod in result:
         yield tuple(prod)
+
 
 def str2bool(string):
     if string.lower() in ("yes", "true"):
@@ -247,15 +254,16 @@ def str2bool(string):
         return False
     raise Exception("Invalid value of the 'zero_rates_when_idle' param, use one of the ('true', 'yes', 'false', 'no')")
 
+
 def metric_init(params):
     ''' Create the metric definition object '''
     global descriptors, stats, vhost, username, password, urlstring, url_template, compiled_results, STATS, vhosts, zero_rates_when_idle
     if log is None:
-       setup_logging('syslog', params['syslog_facility'], params['log_level'])
+        setup_logging('syslog', params['syslog_facility'], params['log_level'])
     log.info('received the following params: %r' % params)
-    #Set this globally so we can refresh stats
+    # Set this globally so we can refresh stats
     if 'host' not in params:
-        params['host'], params['vhost'],params['username'],params['password'],params['port'] = "localhost", "/", "guest", "guest", "15672"
+        params['host'], params['vhost'], params['username'], params['password'], params['port'] = "localhost", "/", "guest", "guest", "15672"
     if 'zero_rates_when_idle' not in params:
         params['zero_rates_when_idle'] = "false"
 
@@ -268,8 +276,8 @@ def metric_init(params):
 
     zero_rates_when_idle = str2bool(params['zero_rates_when_idle'])
 
-    url = 'http://%s:%s/api/$stats/$vhost' % (host,port)
-    base_url = 'http://%s:%s/api' % (host,port)
+    url = 'http://%s:%s/api/$stats/$vhost' % (host, port)
+    base_url = 'http://%s:%s/api' % (host, port)
     password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
     password_mgr.add_password(None, base_url, username, password)
     handler = urllib2.HTTPBasicAuthHandler(password_mgr)
@@ -278,14 +286,12 @@ def metric_init(params):
     urllib2.install_opener(opener)
     url_template = Template(url)
 
-    refreshStats(stats = STATS, vhosts = vhosts)
+    refreshStats(stats=STATS, vhosts=vhosts)
 
     def metric_handler(name):
         if 15 < time.time() - metric_handler.timestamp:
             metric_handler.timestamp = time.time()
-            return refreshStats(stats = STATS, vhosts = vhosts)
-
-
+            return refreshStats(stats=STATS, vhosts=vhosts)
 
     def create_desc(prop):
         d = {
@@ -300,10 +306,9 @@ def metric_init(params):
             'groups'      : params["metric_group"],
         }
 
-        for k,v in prop.iteritems():
+        for k, v in prop.iteritems():
             d[k] = v
         return d
-
 
     def buildQueueDescriptors():
         for vhost, metric in product(vhosts, QUEUE_METRICS):
@@ -311,14 +316,14 @@ def metric_init(params):
             for queue in queues:
                 name = "{1}{0}{2}{0}{3}".format(METRIC_TOKEN_SEPARATOR, metric, queue, vhost.replace('/', '-'))
                 log.debug(name)
-                d1 = create_desc({'name': name.encode('ascii','ignore'),
-                    'call_back': getQueueStat,
-                    'value_type': 'float',
-                    'units': 'N',
-                    'slope': 'both',
-                    'format': '%f',
-                    'description': 'Queue_Metric',
-                    'groups' : 'rabbitmq,queue'})
+                d1 = create_desc({'name': name.encode('ascii', 'ignore'),
+                                  'call_back': getQueueStat,
+                                  'value_type': 'float',
+                                  'units': 'N',
+                                  'slope': 'both',
+                                  'format': '%f',
+                                  'description': 'Queue_Metric',
+                                  'groups': 'rabbitmq,queue'})
                 log.debug(d1)
                 descriptors.append(d1)
 
@@ -327,14 +332,14 @@ def metric_init(params):
             for node in list_nodes():
                 name = "{1}{0}{2}{0}-".format(METRIC_TOKEN_SEPARATOR, metric, node)
                 log.debug(name)
-                d2 = create_desc({'name': name.encode('ascii','ignore'),
-                    'call_back': getNodeStat,
-                    'value_type': 'float',
-                    'units': 'N',
-                    'slope': 'both',
-                    'format': '%f',
-                    'description': 'Node_Metric',
-                    'groups' : 'rabbitmq,node'})
+                d2 = create_desc({'name': name.encode('ascii', 'ignore'),
+                                  'call_back': getNodeStat,
+                                  'value_type': 'float',
+                                  'units': 'N',
+                                  'slope': 'both',
+                                  'format': '%f',
+                                  'description': 'Node_Metric',
+                                  'groups': 'rabbitmq,node'})
                 log.debug(d2)
                 descriptors.append(d2)
 
@@ -344,14 +349,14 @@ def metric_init(params):
             for exchange in exchanges:
                 name = "{1}{0}{2}{0}{3}".format(METRIC_TOKEN_SEPARATOR, metric, exchange, vhost.replace('/', '-'))
                 log.debug(name)
-                d1 = create_desc({'name': name.encode('ascii','ignore'),
-                    'call_back': getExchangeStat,
-                    'value_type': 'float',
-                    'units': 'N',
-                    'slope': 'both',
-                    'format': '%f',
-                    'description': 'Exchange_Metric',
-                    'groups' : 'rabbitmq,exchange'})
+                d1 = create_desc({'name': name.encode('ascii', 'ignore'),
+                                  'call_back': getExchangeStat,
+                                  'value_type': 'float',
+                                  'units': 'N',
+                                  'slope': 'both',
+                                  'format': '%f',
+                                  'description': 'Exchange_Metric',
+                                  'groups': 'rabbitmq,exchange'})
                 log.debug(d1)
                 descriptors.append(d1)
 
@@ -365,6 +370,7 @@ def metric_init(params):
 
     return descriptors
 
+
 def metric_cleanup():
     pass
 
@@ -373,7 +379,7 @@ def setup_logging(handlers, facility, level):
     global log
 
     log = logging.getLogger('gmond_python_rabbitmq')
-    formatter = logging.Formatter(' | '.join(['%(asctime)s', '%(name)s',  '%(levelname)s', '%(message)s']))
+    formatter = logging.Formatter(' | '.join(['%(asctime)s', '%(name)s', '%(levelname)s', '%(message)s']))
     if handlers in ['syslog', 'both']:
         sh = logging.handlers.SysLogHandler(address='/dev/log', facility=facility)
         sh.setFormatter(formatter)
@@ -388,8 +394,7 @@ def setup_logging(handlers, facility, level):
         'WARNING': logging.WARNING,
         'INFO': logging.INFO,
         'DEBUG': logging.DEBUG,
-        'NOTSET': logging.NOTSET
-        }
+        'NOTSET': logging.NOTSET}
     log.setLevel(lmap[level])
 
 
@@ -428,15 +433,15 @@ def main(argv):
     """ used for testing """
     (opts, args) = parse_args(argv)
     setup_logging(opts.log, opts.log_facility, opts.log_level)
-### in config files we use '/' in vhosts names but we should convert '/' to '-' when calculating a metric
-    parameters = {"vhost":"/", "username":"guest","password":"guest", "metric_group":"rabbitmq",
+    # in config files we use '/' in vhosts names but we should convert '/' to '-' when calculating a metric
+    parameters = {"vhost": "/", "username": "guest", "password": "guest", "metric_group": "rabbitmq",
                   "zero_rates_when_idle": "yes",
                   "host": opts.admin_host, "port": opts.admin_port,
                   "stats": opts.stats.split(','),
                   "vhosts": opts.vhosts.split(',')}
     descriptors = metric_init(parameters)
-    result = refreshStats(stats = parameters['stats'], vhosts = parameters['vhosts'])
-    print '***'*20
+    result = refreshStats(stats=parameters['stats'], vhosts=parameters['vhosts'])
+    print '***' * 20
     if opts.list_only is True:
         print 'nodes:'
         pprint.pprint(list_nodes())
