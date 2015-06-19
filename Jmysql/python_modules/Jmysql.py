85ba8ac9b825a7938f7e9332673f9219e8bf58a2
马建桥 <majianqiao@chinark.com>
2015-06-19 10:59:27 +0800

    中期测试

--- Jmysql/python_modules/Jmysql.py
+++ Jmysql/python_modules/Jmysql.py
@@ -1,22 +1,58 @@
 #!/usr/bin/python
+# -*- coding:utf-8 -*-
 import sys,os
-descriptors = list()
 
-def call_back(name):
+import logging
+logging.basicConfig(level=logging.ERROR, format="%(asctime)s - %(name)s - %(levelname)s\t Thread-%(thread)d - %(message)s", filename='/tmp/mysqlstats.log', filemode='w')
+logging.debug('starting up')
+
+from metrics import throughput_metrics
+from metrics import count_metrics
+from metrics import static_metrics
+
+descriptors 		= list()
+metricTemple 		= (throughput_metrics,count_metrics,static_metrics)
+
+def get_status(name):
 	"""return a metric value."""
 	return name
 
 def metric_init(params):
 	"""Initialize all necessary initialization here."""
 	global descriptors
+	global metricsDefination
 
-	if "param1" in params:
-		p1 = params["param1"]
-	d1 = dict(name="jianqiaoMetric", call_back=call_back, time_max=30, value_type="unit", units='N', slope="both",
-			  format="%u", description="jianqiaoMetric4mysql", groups="example")
-	# print(d1)
+	# 检查params
+	if "host" in params:
+		host = params["host"]
+	else:
+		logging.debug("找不到host定义.检查Jmysql.pyconf")
+		exit(1)
+	if "user" in params:
+		user = params["user"]
+	else:
+		logging.debug("找不到user定义.检查Jmysql.pyconf")
+		exit(1)
+	if "passwd" in params:
+		passwd = params["passwd"]
+	else:
+		logging.debug("passwd.检查Jmysql.pyconf")
+		exit(1)
 
-	descriptors = [d1]
+	for metrics in metricTemple:
+		for defination in metrics:
+			# 定义标准description
+			d = dict(call_back	= get_status,
+					 time_max	= 30,
+					 value_type	= "unit",
+					 units		= "",
+					 format		= "%s",
+					 slope		= "both")
+			d.update(metrics[defination])
+			d.update({"name":defination})
+			logging.debug("generated"+defination)
+			descriptors.append(d)
+
 	return descriptors
 
 def metric_cleanup():
@@ -24,8 +60,11 @@ def metric_cleanup():
 	pass
 
 if __name__ == "__main__":
-	params = {"param1":"param1_value"}
+	params = dict(host="192.168.1.104",
+				  user="autop",
+				  passwd="autop")
 	metric_init(params)
-	for d in descriptors:
-		value = d["call_back"](d["name"])
-		print("value for %s is %s" %(d["name"],value))
+	# for d in descriptors:
+	# 	value = d["call_back"](d["name"])
+	# 	print("value for %s is %s" %(d["name"],value))
+	print(descriptors)
--- Jmysql/python_modules/test.py
+++ Jmysql/python_modules/test.py
@@ -6,33 +6,52 @@
 
 
 # import json
-f = open("last_status","wb")
+# f = open("last_status","wb")
 # str = json.load(f)
 # print(str)
 # print(type(str))
 # f.close()
 
-import mysql.connector
-import json
-
-conn = mysql.connector.connect(user="cupid",
-                               password="everyone2xhfz",
-                               host="192.168.1.104")
-cursor = conn.cursor()
-
-# cursor.execute("SHOW global status like \'Com_select\';")
-# for line in cursor:
-#     print(line)
-# print("=================================================")
+# import mysql.connector
+# import json
+#
+# conn = mysql.connector.connect(user="cupid",
+#                                password="everyone2xhfz",
+#                                host="192.168.1.104")
+# cursor = conn.cursor()
+#
+# # cursor.execute("SHOW global status like \'Com_select\';")
+# # for line in cursor:
+# #     print(line)
+# # print("=================================================")
 # print(dir(cursor))
-cursor.execute("SHOW global status;")
-metricList = cursor.fetchall()
-conn.close()
-metricDict = {}
-print(metricList)
-for m in metricList:
-    metricDict[m[0].encode("utf-8")]=m[1].encode("utf-8")
+# # cursor.execute("SHOW global status;")
+# # metricList = cursor.fetchall()
+# # conn.close()
+# # metricDict = {}
+# # print(metricList)
+# # for m in metricList:
+# #     metricDict[m[0].encode("utf-8")]=m[1].encode("utf-8")
+# #
+# # f.write(json.dumps(metricDict))
+# # f.close()
+# # print(metricDict)
+#
+#
+# cursor.execute("SHOW VARIABLES")
+# print(cursor.fetchall)
+# #variables = dict(((k.lower(), v) for (k,v) in cursor))
+# variables = {}
+# for (k,v) in cursor:
+#     variables[k.lower()] = v
+#
+# # print(variables)
+# try:
+#     raise Exception("test")
+# except Exception,e:
+#     print(e.message)
 
-f.write(json.dumps(metricDict))
-f.close()
-print(metricDict)
+a = {"a":"","b":"123","c":"ccc"}
+b = {"a":"aaa","b":"456"}
+a.update(b)
+print(a)
