8bc8f7c9f6c11cdbcf755fa86ff0b3068c77ac50
马建桥 <majianqiao@chinark.com>
2015-06-19 17:38:08 +0800

    中期测试

--- Jmysql/python_modules/Jmysql.py
+++ Jmysql/python_modules/Jmysql.py
@@ -1,10 +1,5 @@
 # -*- coding:utf-8 -*-
-import logging
-
 import mysql.connector
-
-logging.basicConfig(level=logging.ERROR, format="%(asctime)s - %(name)s - %(levelname)s\t Thread-%(thread)d - %(message)s", filename='/tmp/mysqlstats.log', filemode='w')
-logging.debug('starting up')
 
 from packages.metrics import throughput_metrics
 from packages.metrics import count_metrics
@@ -18,23 +13,22 @@ status		 	= {}
 
 def get_status(name):
 	"""return a metric value."""
-	if name in throughput_metrics:
-		name2key = name[6:-11].lower()
-		if not name.startswith("mysql"):
-			name2key = name[:-11].lower()
-		return status[name2key]
-	elif name in count_metrics:
-		name2key = name[6:].lower()
-		if not name.startswith("mysql"):
-			name2key = name.lower()
-		return status[name2key]
-	elif name in static_metrics:
-		name2key = name[6:].lower()
-		if not name.startswith("mysql"):
-			name2key = name.lower()
-		return variables[name2key]
-	# if name in count_metrics:
-	# 	return status[name[7:].lower()]
+	# if name in throughput_metrics:
+	# 	name2key = name[6:-11].lower()
+	# 	if not name.startswith("mysql"):
+	# 		name2key = name[:-11].lower()
+	# 	return status[name2key]
+	# elif name in count_metrics:
+	# 	name2key = name[6:].lower()
+	# 	if not name.startswith("mysql"):
+	# 		name2key = name.lower()
+	# 	return status[name2key]
+	# elif name in static_metrics:
+	# 	name2key = name[6:].lower()
+	# 	if not name.startswith("mysql"):
+	# 		name2key = name.lower()
+	# 	return variables[name2key]
+	return status["tokudb_txn_commits"]
 
 def metric_init(params):
 	"""Initialize all necessary initialization here."""
@@ -45,13 +39,13 @@ def metric_init(params):
 
 	# 检查params
 	if "host" not in params:
-		logging.debug("找不到host定义.检查Jmysql.pyconf")
+		print("找不到host定义.检查Jmysql.pyconf")
 		exit(1)
 	if "user" not in params:
-		logging.debug("找不到user定义.检查Jmysql.pyconf")
+		print("找不到user定义.检查Jmysql.pyconf")
 		exit(1)
 	if "passwd" not in params:
-		logging.debug("passwd.检查Jmysql.pyconf")
+		print("passwd.检查Jmysql.pyconf")
 		exit(1)
 
 	# 连接mysql，获取状态
@@ -67,28 +61,22 @@ def metric_init(params):
 	cursor.close()
 	conn.close()
 
-	for collect in collectTemple:
-		for metric in collect:
-			# 定义标准description
-			d = dict(call_back	= get_status,
-					 time_max	= 30,
-					 value_type	= "uint",
-					 units		= "",
-					 format		= "%s",
-					 slope		= "both")
-			d.update(collect[metric])
-			d.update({"name":metric})
-			d.update({"title":metric})
-			logging.debug("generated"+metric)
-			descriptors.append(d)
-
+	d1 = dict(name="tokudb_txn_commits",
+			  call_back=get_status,
+			  time_max=30,
+			  value_type="uint",
+			  units="N",
+			  slope="both",
+			  format="%u",
+			  description="test metric")
+	descriptors.append(d1)
 	# print(status)
 	return descriptors
 
 def metric_cleanup():
 	"""Clean up the metric module"""
-	logging.shutdown()
-
+	# logging.shutdown()
+	pass
 if __name__ == "__main__":
 	params = dict(host="192.168.1.104",
 				  user="autop",
