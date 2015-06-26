c221de7a7028b4de066a533272cfbd6498621b42
马建桥 <majianqiao@chinark.com>
2015-06-26 15:16:42 +0800

    中期测试

--- Jmysql/python_modules/Jmysql.py
+++ Jmysql/python_modules/Jmysql.py
@@ -22,7 +22,7 @@ testNum			= 0
 
 
 logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s-%(filename)s[line:%(lineno)d]-%(levelname)s-%(message)s',
+                    format='%(process)d-%(threadName)s-%(asctime)s-%(filename)s[line:%(lineno)d]-%(levelname)s-%(message)s',
                     datefmt='%a, %d %b %Y %H:%M:%S',
                     filename='/tmp/test.log',
                     filemode='w')
