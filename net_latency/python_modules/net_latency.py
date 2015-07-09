f00def7823b3a66fea4936f0ace7fbb4341b43d2
Giorgos Kappes <geokapp@gmail.com>
2015-07-09 12:44:03 +0300

    Added pinger thread and ping timeout

--- net_latency/python_modules/net_latency.py
+++ net_latency/python_modules/net_latency.py
@@ -6,6 +6,11 @@
 #
 import subprocess
 import os
+import time
+import threading
+
+# The worker thread.
+worker = None
 
 def is_number(s):
     try:
@@ -13,36 +18,92 @@ def is_number(s):
         return True
     except ValueError:
         return False
+    
+class PingerThread(threading.Thread):
+    def __init__(self, params):
+        threading.Thread.__init__(self)
+        self.running = False
+        self.stopping = False
+        self.refresh_rate = int(params['refresh_rate'])
+        self.target = params['target']
+        if not self.target:
+            # We choose the host's gateway as the default target.
+            self.target = "$(ip route show | grep default | awk '{ print $3 }')"
 
-def measure_latency(name):
+        self.latency = 0
+        self.lock = threading.Lock()
+
+    def shutdown(self):
+        self.stopping = True
+        if not self.running:
+            return
+        self.join()
+
+    def run(self):
+        self.running = True
+        while not self.stopping:
+            time.sleep(self.refresh_rate)
+            self.refresh_metrics()
+
+        self.running = False
+      
+    def measure_latency(self):
         try:
-		target = "$(ip route show | grep default | awk '{ print $3 }')"
-		command = "ping -c 5 "+target+" | tail -1| awk -F '/' '{print $5}'"
-                f = os.popen(command)
-                res = f.read()
-		if is_number(res) == False:
-			return 0
-        except IOError:
+            command = "ping -c 5 -W 2 "+self.target+" | tail -1| awk -F '/' '{print $5}'"
+            f = os.popen(command)
+            res = f.read()
+            if is_number(res) == False:
                 return 0
+        except IOError:
+            return 0
+        
+        return int(float(res) * 1000)
 
-	return int(float(res) * 1000)
+    def refresh_metrics(self):
+        self.lock.acquire()
+        self.latency = self.measure_latency()
+        self.lock.release()
+        
+    def get_latency(self, name):
+        self.lock.acquire()
+        l = self.latency
+        self.lock.release()
+        return l
+
+def create_descriptors(params):
+    global descriptors
+
+    d1 = {'name': 'net_latency',
+          'call_back': get_latency,
+          'value_type': 'uint',
+          'units': 'microseconds',
+          'slope': 'both',
+          'format': '%u',
+          'description': 'Network Latency',
+          'groups': 'network' }
+    
+    descriptors = [d1]
+    return descriptors
 
 def metric_init(params):
-	global descriptors
-	
-	d1 = {'name': 'net_latency',
-		'call_back': measure_latency,
-		'value_type': 'uint',
-		'units': 'microseconds',
-		'slope': 'both',
-		'format': '%u',
-		'description': 'Network Latency',
-		'groups': 'network' }
-	
-	descriptors = [d1]
-	return descriptors
+    descriptors = create_descriptors(params)
+    global worker
+    if worker is not None:
+        raise Exception('Worker thread exists')
+        
+    worker = PingerThread(params)
+    worker.refresh_metrics()
+    worker.start()
+    return descriptors
 
+def get_latency(name):
+    global worker
+    return worker.get_latency(name)
+    
 def metric_cleanup():
-	'''Clean up the metric module.'''
-	pass
+    global worker
+    if worker is not None:
+        worker.shutdown()
+
+    pass
     
