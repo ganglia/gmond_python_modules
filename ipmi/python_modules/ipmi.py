e28c7c3964d6d248b345985affffc183ae515312
Kilian Lackhove <kilian@lackhove.de>
2016-02-12 15:29:58 +0100

    IPMI: run autopep8. no logic changes

--- ipmi/python_modules/ipmi.py
+++ ipmi/python_modules/ipmi.py
@@ -6,13 +6,14 @@ import string
 import subprocess
 
 METRICS = {
-    'time' : 0,
-    'data' : {}
+    'time': 0,
+    'data': {}
 }
 
 METRICS_CACHE_MAX = 5
 
-stats_pos = {} 
+stats_pos = {}
+
 
 def get_metrics():
     """Return all metrics"""
@@ -21,10 +22,10 @@ def get_metrics():
 
     if (time.time() - METRICS['time']) > METRICS_CACHE_MAX:
 
-	new_metrics = {}
-	units = {}
+        new_metrics = {}
+        units = {}
 
-	command = ['ipmitool', 'sdr']
+        command = ['ipmitool', 'sdr']
 
         p = subprocess.Popen(command,
                              stdout=subprocess.PIPE).communicate()[0][:-1]
@@ -32,13 +33,14 @@ def get_metrics():
         for i, v in enumerate(p.split("\n")):
             data = v.split("|")
             try:
-                metric_name = data[0].strip().lower().replace("+", "").replace(" ", "_")
+                metric_name = data[0].strip().lower().replace(
+                    "+", "").replace(" ", "_")
                 data[1] = data[1].strip().split(" ")
                 value = data[1][0].strip()
                 unit = " ".join(data[1][1:])
 
                 # Skip missing sensors
-                if re.search("(0x)", value ) or value == 'na':
+                if re.search("(0x)", value) or value == 'na':
                     continue
 
                 # Extract out a float value
@@ -54,8 +56,8 @@ def get_metrics():
                 continue
             except IndexError:
                 continue
-		
-	METRICS = {
+
+        METRICS = {
             'time': time.time(),
             'data': new_metrics,
             'units': units
@@ -73,11 +75,13 @@ def get_value(name):
 
     return metrics['data'][name]
 
+
 def create_desc(skel, prop):
     d = skel.copy()
-    for k,v in prop.iteritems():
+    for k, v in prop.iteritems():
         d[k] = v
     return d
+
 
 def metric_init(params):
     global descriptors, metric_map, Desc_Skel
@@ -85,45 +89,45 @@ def metric_init(params):
     descriptors = []
 
     Desc_Skel = {
-        'name'        : 'XXX',
-        'call_back'   : get_value,
-        'time_max'    : 60,
-        'value_type'  : 'float',
-        'format'      : '%.5f',
-        'units'       : 'count/s',
-        'slope'       : 'both', # zero|positive|negative|both
-        'description' : 'XXX',
-        'groups'      : 'XXX',
-        }
+        'name': 'XXX',
+        'call_back': get_value,
+        'time_max': 60,
+        'value_type': 'float',
+        'format': '%.5f',
+        'units': 'count/s',
+        'slope': 'both',  # zero|positive|negative|both
+        'description': 'XXX',
+        'groups': 'XXX',
+    }
 
     metrics = get_metrics()[0]
-    
-    for item in metrics['data']:
-	descriptors.append(create_desc(Desc_Skel, {
-		"name"       	: params['metric_prefix'] + "_" + item,
-		'groups'	: params['metric_prefix'],
-		'units'		: metrics['units'][item]
-		}))
 
+    for item in metrics['data']:
+        descriptors.append(create_desc(Desc_Skel, {
+            "name"       	: params['metric_prefix'] + "_" + item,
+            'groups'	: params['metric_prefix'],
+            'units'		: metrics['units'][item]
+        }))
 
     return descriptors
+
 
 def metric_cleanup():
     '''Clean up the metric module.'''
     pass
 
-#This code is for debugging and unit testing
+# This code is for debugging and unit testing
 if __name__ == '__main__':
-    
+
     params = {
-	"metric_prefix" : "ipmi",
-	"ipmi_ip" : "10.1.2.3",
-	"username"  : "ADMIN",
-	"password"  : "secret",
-	"level" : "USER",
-	"ipmitool_bin" : "/usr/bin/ipmitool",
-	"timeout_bin" : "/usr/bin/timeout"
-	}
+        "metric_prefix": "ipmi",
+        "ipmi_ip": "10.1.2.3",
+        "username": "ADMIN",
+        "password": "secret",
+        "level": "USER",
+        "ipmitool_bin": "/usr/bin/ipmitool",
+        "timeout_bin": "/usr/bin/timeout"
+    }
     descriptors = metric_init(params)
 
     while True:
